52c0f03220f5f2bbedc18e26bfdd9dc1e34efc19
Zhihao Chen <zh_chen@tju.edu.cn>
2020-10-13 00:20:06 +0800

    Add files via upload

---
+++ utils/losses.py
@@ -0,0 +1,312 @@
+import torch
+from torch.nn import functional as F
+import numpy as np
+from torch.autograd import Variable
+try:
+    from itertools import  ifilterfalse
+except ImportError: # py3k
+    from itertools import  filterfalse as ifilterfalse
+
+def dice_loss(score, target):
+    target = target.float()
+    smooth = 1e-5
+    intersect = torch.sum(score * target)
+    y_sum = torch.sum(target)
+    z_sum = torch.sum(score)
+    loss = (2 * intersect + smooth) / (z_sum + y_sum + smooth)
+    loss = 1 - loss
+    return loss
+
+def entropy_loss(p,C=2):
+    ## p N*C*W*H*D
+    y1 = -1*torch.sum(p*torch.log(p+1e-6), dim=1)/torch.tensor(np.log(C)).cuda()
+    ent = torch.mean(y1)
+
+    return ent
+
+def softmax_dice_loss(input_logits, target_logits):
+    """Takes softmax on both sides and returns MSE loss
+
+    Note:
+    - Returns the sum over all examples. Divide by the batch size afterwards
+      if you want the mean.
+    - Sends gradients to inputs but not the targets.
+    """
+    assert input_logits.size() == target_logits.size()
+    input_softmax = F.softmax(input_logits, dim=1)
+    target_softmax = F.softmax(target_logits, dim=1)
+    n = input_logits.shape[1]
+    dice = 0
+    for i in range(0, n):
+        dice += dice_loss(input_softmax[:, i], target_softmax[:, i])
+    mean_dice = dice / n
+
+    return mean_dice
+
+
+def entropy_loss_map(p, C=2):
+    ent = -1*torch.sum(p * torch.log(p + 1e-6), dim=1, keepdim=True)/torch.tensor(np.log(C)).cuda()
+    return ent
+
+def softmax_mse_loss(input_logits, target_logits):
+    """Takes softmax on both sides and returns MSE loss
+
+    Note:
+    - Returns the sum over all examples. Divide by the batch size afterwards
+      if you want the mean.
+    - Sends gradients to inputs but not the targets.
+    """
+    assert input_logits.size() == target_logits.size()
+    input_softmax = F.softmax(input_logits, dim=1)
+    target_softmax = F.softmax(target_logits, dim=1)
+
+    # num_classes = input_logits.size()[1]
+    return F.mse_loss(input_softmax, target_softmax, reduction='mean')
+
+def sigmoid_mse_loss(input_logits, target_logits):
+    """Takes sigmoid on both sides and returns MSE loss
+
+    Note:
+    - Returns the sum over all examples. Divide by the batch size afterwards
+      if you want the mean.
+    - Sends gradients to inputs but not the targets.
+    """
+    assert input_logits.size() == target_logits.size()
+    input_sigmoid = F.sigmoid(input_logits)
+    target_sigmoid = F.sigmoid(target_logits)
+
+    # num_classes = input_logits.size()[1]
+    return F.mse_loss(input_sigmoid, target_sigmoid, reduction='mean')
+
+def mse_loss(input, target):
+    return F.mse_loss(input, target, reduction='mean')
+
+# def softmax_kl_loss(input_logits, target_logits):
+#     """Takes softmax on both sides and returns KL divergence
+#
+#     Note:
+#     - Returns the sum over all examples. Divide by the batch size afterwards
+#       if you want the mean.
+#     - Sends gradients to inputs but not the targets.
+#     """
+#     assert input_logits.size() == target_logits.size()
+#     input_log_softmax = F.log_softmax(input_logits, dim=1)
+#     target_softmax = F.softmax(target_logits, dim=1)
+#
+#     # return F.kl_div(input_log_softmax, target_softmax)
+#     kl_div = F.kl_div(input_log_softmax, target_softmax, reduction='none')
+#     # mean_kl_div = torch.mean(0.2*kl_div[:,0,...]+0.8*kl_div[:,1,...])
+#     return kl_div
+def kl_loss(input_logits, target_logits):
+    """Takes softmax on both sides and returns KL divergence
+
+    Note:
+    - Returns the sum over all examples. Divide by the batch size afterwards
+      if you want the mean.
+    - Sends gradients to inputs but not the targets.
+    """
+    assert input_logits.size() == target_logits.size()
+    return F.kl_div(input_logits, target_logits, reduction='mean')
+
+def softmax_kl_loss(input_logits, target_logits):
+    """Takes softmax on both sides and returns KL divergence
+
+    Note:
+    - Returns the sum over all examples. Divide by the batch size afterwards
+      if you want the mean.
+    - Sends gradients to inputs but not the targets.
+    """
+    assert input_logits.size() == target_logits.size()
+    input_log_softmax = F.log_softmax(input_logits, dim=1)
+    target_softmax = F.softmax(target_logits, dim=1)
+    return F.kl_div(input_log_softmax, target_softmax, reduction='mean')
+
+def symmetric_mse_loss(input1, input2):
+    """Like F.mse_loss but sends gradients to both directions
+
+    Note:
+    - Returns the sum over all examples. Divide by the batch size afterwards
+      if you want the mean.
+    - Sends gradients to both input1 and input2.
+    """
+    assert input1.size() == input2.size()
+    return torch.mean((input1 - input2)**2)
+
+def bce2d_new(input, target, reduction=None):
+    assert(input.size() == target.size())
+    pos = torch.eq(target, 1).float()
+    neg = torch.eq(target, 0).float()
+    # ing = ((torch.gt(target, 0) & torch.lt(target, 1))).float()
+
+    num_pos = torch.sum(pos)
+    num_neg = torch.sum(neg)
+    num_total = num_pos + num_neg
+
+    alpha = num_neg  / num_total
+    beta = 1.1 * num_pos  / num_total
+    # target pixel = 1 -> weight beta
+    # target pixel = 0 -> weight 1-beta
+    weights = alpha * pos + beta * neg
+
+    return F.binary_cross_entropy_with_logits(input, target, weights, reduction=reduction)
+
+# --------------------------- BINARY Lovasz LOSSES ---------------------------
+def lovasz_hinge_weight(logits, labels, weights, per_image=True, ignore=None):
+    """
+    Binary Lovasz hinge loss
+      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
+      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+      per_image: compute the loss per image instead of per batch
+      ignore: void class id
+    """
+    if per_image:
+        loss = mean(lovasz_hinge_flat_weight(*flatten_binary_scores_weight(log.unsqueeze(0), lab.unsqueeze(0), weight.unsqueeze(0), ignore))
+                          for log, lab, weight in zip(logits, labels, weights))
+    else:
+        loss = lovasz_hinge_flat_weight(*flatten_binary_scores_weight(logits, labels, weights, ignore))
+    return loss
+
+def lovasz_hinge_flat_weight(logits, labels, weights):
+    """
+    Binary Lovasz hinge loss
+      logits: [P] Variable, logits at each prediction (between -\infty and +\infty)
+      labels: [P] Tensor, binary ground truth labels (0 or 1)
+      ignore: label to ignore
+    """
+    if len(labels) == 0:
+        # only void pixels, the gradients should be 0
+        return logits.sum() * 0.
+    signs = 2. * labels.float() - 1.
+    errors = (1. - logits * Variable(signs))
+    errors_sorted, perm = torch.sort(errors, dim=0, descending=True)
+    perm = perm.data
+    gt_sorted = labels[perm]
+    weight_sort = weights[perm]
+    grad = lovasz_grad(gt_sorted) * weight_sort
+    loss = torch.dot(F.relu(errors_sorted), Variable(grad))
+    return loss
+
+
+def flatten_binary_scores_weight(scores, labels, weights, ignore=None):
+    """
+    Flattens predictions in the batch (binary case)
+    Remove labels equal to 'ignore'
+    """
+    scores = scores.view(-1)
+    labels = labels.view(-1)
+    weights = weights.view(-1)
+    if ignore is None:
+        return scores, labels, weights
+    valid = (labels != ignore)
+    vscores = scores[valid]
+    vlabels = labels[valid]
+    vweights = weights[valid]
+    return vscores, vlabels, vweights
+
+def lovasz_hinge(logits, labels, per_image=True, ignore=None):
+    """
+    Binary Lovasz hinge loss
+      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
+      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+      per_image: compute the loss per image instead of per batch
+      ignore: void class id
+    """
+    if per_image:
+        loss = mean(lovasz_hinge_flat(*flatten_binary_scores(log.unsqueeze(0), lab.unsqueeze(0), ignore))
+                          for log, lab in zip(logits, labels))
+    else:
+        loss = lovasz_hinge_flat(*flatten_binary_scores(logits, labels, ignore))
+    return loss
+
+
+def lovasz_hinge_flat(logits, labels):
+    """
+    Binary Lovasz hinge loss
+      logits: [P] Variable, logits at each prediction (between -\infty and +\infty)
+      labels: [P] Tensor, binary ground truth labels (0 or 1)
+      ignore: label to ignore
+    """
+    if len(labels) == 0:
+        # only void pixels, the gradients should be 0
+        return logits.sum() * 0.
+    signs = 2. * labels.float() - 1.
+    errors = (1. - logits * Variable(signs))
+    errors_sorted, perm = torch.sort(errors, dim=0, descending=True)
+    perm = perm.data
+    gt_sorted = labels[perm]
+    grad = lovasz_grad(gt_sorted)
+    loss = torch.dot(F.relu(errors_sorted), Variable(grad))
+    return loss
+
+
+def flatten_binary_scores(scores, labels, ignore=None):
+    """
+    Flattens predictions in the batch (binary case)
+    Remove labels equal to 'ignore'
+    """
+    scores = scores.view(-1)
+    labels = labels.view(-1)
+    if ignore is None:
+        return scores, labels
+    valid = (labels != ignore)
+    vscores = scores[valid]
+    vlabels = labels[valid]
+    return vscores, vlabels
+
+
+class StableBCELoss(torch.nn.modules.Module):
+    def __init__(self):
+         super(StableBCELoss, self).__init__()
+    def forward(self, input, target):
+         neg_abs = - input.abs()
+         loss = input.clamp(min=0) - input * target + (1 + neg_abs.exp()).log()
+         return loss.mean()
+
+
+def binary_xloss(logits, labels, ignore=None):
+    """
+    Binary Cross entropy loss
+      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
+      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+      ignore: void class id
+    """
+    logits, labels = flatten_binary_scores(logits, labels, ignore)
+    loss = StableBCELoss()(logits, Variable(labels.float()))
+    return loss
+
+def lovasz_grad(gt_sorted):
+    """
+    Computes gradient of the Lovasz extension w.r.t sorted errors
+    See Alg. 1 in paper
+    """
+    p = len(gt_sorted)
+    gts = gt_sorted.sum()
+    intersection = gts - gt_sorted.float().cumsum(0)
+    union = gts + (1 - gt_sorted).float().cumsum(0)
+    jaccard = 1. - intersection / union
+    if p > 1: # cover 1-pixel case
+        jaccard[1:p] = jaccard[1:p] - jaccard[0:-1]
+    return jaccard
+
+def mean(l, ignore_nan=False, empty=0):
+    """
+    nanmean compatible with generators.
+    """
+    l = iter(l)
+    if ignore_nan:
+        l = ifilterfalse(isnan, l)
+    try:
+        n = 1
+        acc = next(l)
+    except StopIteration:
+        if empty == 'raise':
+            raise ValueError('Empty mean')
+        return empty
+    for n, v in enumerate(l, 2):
+        acc += v
+    if n == 1:
+        return acc
+    return acc / n
+
+def isnan(x):
+    return x != x

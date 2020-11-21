26d921c5007e87b1a13d5f7d178438e8b9ae82e2
Zhihao Chen <zh_chen@tju.edu.cn>
2020-11-21 16:27:12 +0800

    Update train.py

--- train.py
+++ train.py
@@ -15,25 +15,30 @@ from torchvision import transforms
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
+from torchvision.utils import make_grid
 import torchvision.utils as vutils
 
-from networks.MTMT import build_model
+from networks.vnet import VNet
+from networks.vnet2d import VNet2d
+from networks.FPN import FPN
+from networks.EGNet_subitizing import build_model
+from dataloaders import utils
 from utils import ramps, losses
-from dataloaders.SBU import SBU
-from dataloaders.ISIC import ISIC
-import dataloaders.joint_transforms_edge as joint_transforms
-from utils.util import AverageMeter, TwoStreamBatchSampler, relabel_dataset
+from dataloaders.la_heart import LAHeart, RandomCrop, CenterCrop, RandomRotFlip, ToTensor, TwoStreamBatchSampler
+from dataloaders.SBU import SBU, relabel_dataset
+from dataloaders import joint_transforms
+from utils.util import AverageMeter
 parser = argparse.ArgumentParser()
-# parser.add_argument('--root_path', type=str, default='/media/data/chenzhihao/datasets/ISIC-2017', help='Name of Experiment')
-parser.add_argument('--root_path', type=str, default='/media/data/chenzhihao/datasets/ISIC_2018_Skin_Lesion', help='Name of Experiment')
-parser.add_argument('--exp', type=str,  default='MTMT', help='model_name')
-parser.add_argument('--label_consistency', type=int,  default=1, help='consistency loss for label data')
-parser.add_argument('--max_iterations', type=int,  default=7000, help='maximum epoch number to train')
+# parser.add_argument('--root_path', type=str, default='/home/ext/chenzhihao/Datasets/union-shadow_extend/union-Train', help='Name of Experiment')
+parser.add_argument('--root_path', type=str, default='/home/ext/chenzhihao/Datasets/SBU_USR_manShadow/union-Train', help='Name of Experiment')
+parser.add_argument('--exp', type=str,  default='EGNet', help='model_name')
+parser.add_argument('--max_iterations', type=int,  default=10000, help='maximum epoch number to train')
 parser.add_argument('--batch_size', type=int, default=6, help='batch_size per gpu')
-parser.add_argument('--labeled_bs', type=int, default=3, help='labeled_batch_size per gpu')
+parser.add_argument('--labeled_bs', type=int, default=4, help='labeled_batch_size per gpu')
 parser.add_argument('--base_lr', type=float,  default=0.005, help='maximum epoch number to train')
 parser.add_argument('--lr_decay', type=float,  default=0.9, help='learning rate decay')
 parser.add_argument('--edge', type=float, default='10', help='edge learning weight')
+parser.add_argument('--deterministic', type=int,  default=0, help='whether use deterministic training')
 parser.add_argument('--seed', type=int,  default=1337, help='random seed')
 parser.add_argument('--gpu', type=str,  default='1', help='GPU to use')
 ### costs
@@ -42,12 +47,13 @@ parser.add_argument('--consistency_type', type=str,  default="mse", help='consis
 parser.add_argument('--consistency', type=float,  default=1, help='consistency')
 parser.add_argument('--consistency_rampup', type=float,  default=7.0, help='consistency_rampup')
 parser.add_argument('--scale', type=int,  default=416, help='batch size of 8 with resolution of 416*416 is exactly OK')
+parser.add_argument('--subitizing', type=float,  default=1, help='subitizing loss weight')
+parser.add_argument('--repeat', type=int,  default=3, help='repeat')
 args = parser.parse_args()
 
 train_data_path = args.root_path
 # snapshot_path = "../model_SBU_BDRAR/torch040_baseline_allCon_resample6-"+str(args.labeled_bs)+"_con-"+str(args.consistency)+"/" + args.exp + "/"
-# snapshot_path = "../model_SBU_EGNet/baselineC64_DSS_unlabelcons/"+'edge'+str(args.edge)+'lr'+str(args.base_lr)+'consistency'+str(args.consistency)+'repeat'+str(args.repeat)+'/'
-snapshot_path = "../models_ISIC2018/MTMT_baseline_v2"
+snapshot_path = "../model_SBU_EGNet/baselineC64_DSS_unlabelcons/repeat"+str(args.repeat)+'_edge'+str(args.edge)+'lr'+str(args.base_lr)+'consistency'+str(args.consistency)+'subitizing'+str(args.subitizing)+'/'
 tmp_path = 'tmp_see'
 if not os.path.exists(tmp_path):
     os.mkdir(tmp_path)
@@ -59,7 +65,18 @@ base_lr = args.base_lr
 labeled_bs = args.labeled_bs
 lr_decay = args.lr_decay
 loss_record = 0
-cudnn.benchmark = True
+
+if args.deterministic:
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+else:
+    cudnn.benchmark = True
+
+num_classes = 2
 
 def get_current_consistency_weight(epoch):
     # Consistency ramp-up from https://arxiv.org/abs/1610.02242
@@ -73,12 +90,16 @@ def update_ema_variables(model, ema_model, alpha, global_step):
 
 def create_model(ema=False):
     # Network definition
-    net = build_model()
-    net = net.cuda()
     if ema:
-        for param in net.parameters():
+        net = build_model(ema=True)
+        net_cuda = net.cuda()
+        for param in net_cuda.parameters():
             param.detach_()
-    return net
+    else:
+        net = build_model()
+        net_cuda = net.cuda()
+
+    return net_cuda
 
 if __name__ == "__main__":
     ## make logger file
@@ -105,16 +126,14 @@ if __name__ == "__main__":
     ])
     img_transform = transforms.Compose([
         transforms.ToTensor(),
-        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # suitable for SBU
-        transforms.Normalize([0.723, 0.616, 0.569], [0.169, 0.177, 0.197])  # suitable for ISIC2017
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
     target_transform = transforms.ToTensor()
     to_pil = transforms.ToPILImage()
 
+    db_train = SBU(root=train_data_path, joint_transform=joint_transform, transform=img_transform, target_transform=target_transform, mod='union', multi_task=True, edge=True)
 
-    db_train = ISIC(root=train_data_path, joint_transform=joint_transform, transform=img_transform, target_transform=target_transform, mod='union', edge=True)# set edge=True & mod='union' in MTMT
-
-    labeled_idxs, unlabeled_idxs = relabel_dataset(db_train)
+    labeled_idxs, unlabeled_idxs = relabel_dataset(db_train, edge_able=True)
     batch_sampler = TwoStreamBatchSampler(labeled_idxs, unlabeled_idxs, batch_size, batch_size-labeled_bs)
     def worker_init_fn(worker_id):
         random.seed(args.seed+worker_id)
@@ -122,6 +141,7 @@ if __name__ == "__main__":
 
     model.train()
     ema_model.train()
+    # ema_model.eval()
     # optimizer = optim.SGD(model.parameters(), lr=base_lr, momentum=0.9, weight_decay=0.0001)
     optimizer = optim.SGD([
         {'params': [param for name, param in model.named_parameters() if name[-4:] == 'bias'],
@@ -130,9 +150,12 @@ if __name__ == "__main__":
          'lr': base_lr, 'weight_decay': 0.0001}
     ], momentum=0.9)
 
+
     if args.consistency_type == 'mse':
+        # consistency_criterion = losses.softmax_mse_loss
         consistency_criterion = losses.sigmoid_mse_loss
     elif args.consistency_type == 'kl':
+        # consistency_criterion = losses.softmax_kl_loss
         consistency_criterion = F.kl_div
     else:
         assert False, args.consistency_type
@@ -147,6 +170,10 @@ if __name__ == "__main__":
     for epoch_num in tqdm(range(max_epoch), ncols=70):
         time1 = time.time()
         shadow_loss2_record, shadow_con_loss2_record, edge_loss_record, edge_con_loss_record = AverageMeter(), AverageMeter(), AverageMeter(), AverageMeter()
+        subitizing_loss_record = AverageMeter()
+        # loss2_h2l_record, loss3_h2l_record, loss4_h2l_record = AverageMeter(), AverageMeter(), AverageMeter()
+        # loss1_l2h_record, loss2_l2h_record, loss3_l2h_record = AverageMeter(), AverageMeter(), AverageMeter()
+        # loss4_l2h_record, consistency_record = AverageMeter(), AverageMeter()
         for i_batch, sampled_batch in enumerate(trainloader):
             time2 = time.time()
             optimizer.param_groups[0]['lr'] = 2 * base_lr * (1 - float(iter_num) / max_iterations
@@ -154,25 +181,25 @@ if __name__ == "__main__":
             optimizer.param_groups[1]['lr'] = base_lr * (1 - float(iter_num) / max_iterations
                                                             ) ** lr_decay
             # print('fetch data cost {}'.format(time2-time1))
-            image_batch, label_batch, edge_batch = sampled_batch['image'], sampled_batch['label'], sampled_batch['edge']
-            image_batch, label_batch, edge_batch = image_batch.cuda(), label_batch.cuda(), edge_batch.cuda()
+            image_batch, label_batch, edge_batch, number_per_batch = sampled_batch['image'], sampled_batch['label'], sampled_batch['edge'], sampled_batch['number_per']
+            image_batch, label_batch, edge_batch, number_per_batch = image_batch.cuda(), label_batch.cuda(), edge_batch.cuda(), number_per_batch.cuda()
 
             noise = torch.clamp(torch.randn_like(image_batch) * 0.1, -0.2, 0.2)
             ema_inputs = image_batch + noise
-            up_edge, up_shadow, up_shadow_final = model(image_batch)
+            up_edge, up_shadow, up_subitizing, up_shadow_final = model(image_batch)
             with torch.no_grad():
-                up_edge_ema, up_shadow_ema, up_shadow_final_ema = ema_model(ema_inputs)
+                up_edge_ema, up_shadow_ema, up_subitizing_ema, up_shadow_final_ema = ema_model(ema_inputs)
 
             ## calculate the loss
+            ## subitizing loss
+            subitizing_loss = losses.sigmoid_mse_loss(up_subitizing[:labeled_bs], number_per_batch[:labeled_bs])
+            subitizing_con_loss = losses.sigmoid_mse_loss(up_subitizing[labeled_bs:], up_subitizing_ema[labeled_bs:])
             ## edge loss
             edge_loss = []
             edge_con_loss = []
             for (ix, ix_ema) in zip(up_edge, up_edge_ema):
                 edge_loss.append(losses.bce2d_new(ix[:labeled_bs], edge_batch[:labeled_bs], reduction='mean'))
-                if args.label_consistency:
-                    edge_con_loss.append(consistency_criterion(ix, ix_ema))
-                else:
-                    edge_con_loss.append(consistency_criterion(ix[labeled_bs:], ix_ema[labeled_bs:]))
+                edge_con_loss.append(consistency_criterion(ix[labeled_bs:], ix_ema[labeled_bs:]))
             edge_loss = sum(edge_loss)
             edge_con_loss = sum(edge_con_loss)
             shadow_loss1 = []
@@ -181,23 +208,17 @@ if __name__ == "__main__":
             shadow_con_loss2 = []
             for (ix, ix_ema) in zip(up_shadow, up_shadow_ema):
                 shadow_loss1.append(F.binary_cross_entropy_with_logits(ix[:labeled_bs], label_batch[:labeled_bs], reduction='mean'))
-                if args.label_consistency:
-                    shadow_con_loss1.append(consistency_criterion(ix, ix_ema))
-                else:
-                    shadow_con_loss1.append(consistency_criterion(ix[labeled_bs:], ix_ema[labeled_bs:]))
+                shadow_con_loss1.append(consistency_criterion(ix[labeled_bs:], ix_ema[labeled_bs:]))
 
             for (ix, ix_ema) in zip(up_shadow_final, up_shadow_final_ema):
                 shadow_loss2.append(F.binary_cross_entropy_with_logits(ix[:labeled_bs], label_batch[:labeled_bs], reduction='mean'))
-                if args.label_consistency:
-                    shadow_con_loss2.append(consistency_criterion(ix, ix_ema))
-                else:
-                    shadow_con_loss2.append(consistency_criterion(ix[labeled_bs:], ix_ema[labeled_bs:]))
+                shadow_con_loss2.append(consistency_criterion(ix[labeled_bs:], ix_ema[labeled_bs:]))
 
             shadow_loss = sum(shadow_loss1) + sum(shadow_loss2)
-            supervised_loss = shadow_loss + edge_loss * args.edge
+            supervised_loss = shadow_loss + edge_loss * args.edge + subitizing_loss*args.subitizing
 
             consistency_weight = get_current_consistency_weight(epoch_num)
-            consistency_loss = consistency_weight * (edge_con_loss + sum(shadow_con_loss1) + sum(shadow_con_loss2))
+            consistency_loss = consistency_weight * (edge_con_loss + sum(shadow_con_loss1) + sum(shadow_con_loss2) + subitizing_con_loss)
 
             loss = supervised_loss + consistency_loss
 
@@ -213,15 +234,16 @@ if __name__ == "__main__":
             # loss_all_record.update(loss.item(), batch_size)
             shadow_loss2_record.update(shadow_loss2[-1].item(), labeled_bs)
             edge_loss_record.update(edge_loss.item(), labeled_bs)
-            shadow_con_loss2_record.update(shadow_con_loss2[-1].item(), batch_size)
-            edge_con_loss_record.update(edge_con_loss.item(), batch_size)
+            shadow_con_loss2_record.update(shadow_con_loss2[-1].item(), batch_size-labeled_bs)
+            edge_con_loss_record.update(edge_con_loss.item(), batch_size-labeled_bs)
+            subitizing_loss_record.update(subitizing_loss, labeled_bs)
 
-            logging.info('iteration %d : shadow_f : %f5 , edge: %f5 , shadow_f_con: %f5  edge_con: %f5 loss_weight: %f5, lr: %f5' %
-                         (iter_num, shadow_loss2_record.avg, edge_loss_record.avg, shadow_con_loss2_record.avg,edge_con_loss_record.avg, consistency_weight, optimizer.param_groups[1]['lr']))
+            logging.info('iteration %d : shadow_f : %f5 , edge: %f5 , subitizing: %f5, shadow_f_con: %f5  edge_con: %f5 loss_weight: %f5, lr: %f5' %
+                         (iter_num, shadow_loss2_record.avg, edge_loss_record.avg, subitizing_loss_record.avg, shadow_con_loss2_record.avg,edge_con_loss_record.avg, consistency_weight, optimizer.param_groups[1]['lr']))
             loss_record = 'iteration %d : shadow_f : %f5 , edge: %f5 , shadow_f_con: %f5  edge_con: %f5 loss_weight: %f5, lr: %f5' % \
                          (iter_num, shadow_loss2_record.avg, edge_loss_record.avg, shadow_con_loss2_record.avg,edge_con_loss_record.avg, consistency_weight, optimizer.param_groups[1]['lr'])
 
-            if iter_num % 500 == 0:
+            if iter_num % 200 == 0:
                 vutils.save_image(torch.sigmoid(up_shadow_final[-1].data), tmp_path + '/iter%d-d_predict_f.jpg' % iter_num, normalize=True,
                                   padding=0)
                 vutils.save_image(torch.sigmoid(up_shadow_final_ema[-1].data),
@@ -248,6 +270,6 @@ if __name__ == "__main__":
     # torch.save(ema_model.state_dict(), save_mode_path_ema)
     logging.info("save model to {}".format(save_mode_path))
     writer.close()
-    with open('record/loss_record.txt', 'a') as f:
+    with open('record/loss_record_EGNet_MT.txt', 'a') as f:
         f.write(snapshot_path+' ')
         f.write(str(loss_record)+'\r\n')
